54dc0378a8b924220a39f3bd8191eb794dc37803
Myroslav <mdmukhivskyi@gmail.com>
2022-08-04 22:55:10 +0300

    arr and obj

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const arr = ['a', 'pizduk', 'c'];
+const arrObj = {
+    a: 'a',
+    1: 'b',
+    2: 'c'
+};
+
+const a = 'a';
+
+arrObj.a = "Artem";
+console.log(arr[1]);
+console.log(arrObj[a]);
