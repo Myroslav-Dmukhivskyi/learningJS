8573cb1989a79b58528d6e736806b127f536f1fe
Myroslav <mdmukhivskyi@gmail.com>
2022-08-18 14:52:10 +0300

    Secont task

--- index.js
+++ index.js
@@ -10,9 +10,36 @@ const personalMovieDB = {
   private: false 
 };
 
-const movieName = prompt('Один из последних просмотренных фильмов?', ''),
- 
- movieGrade = prompt('На сколько оцените его?', '');
+// const firstName = prompt('Один из последних просмотренных фильмов?', ''),
+//       firstGrade = +prompt('На сколько оцените его?', ''),
+//       secondName = prompt('Один из последних просмотренных фильмов?', ''),
+//       stcondGrade = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[movieName] = movieGrade;
+// personalMovieDB.movies[firstName] = firstGrade;
+// personalMovieDB.movies[secondName] = stcondGrade;
 
+for( let i = 0; i < 2; i++){
+    const a= prompt('Один из последних просмотренных фильмов?', ''),
+       b= +prompt('На сколько оцените его?', '');
+
+        if(a != null && b != null && a!= '' && b != '' && a.length < 50){
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        }else{
+            console.log('error');
+            i--;
+        }
+
+}
+
+
+// console.log(personalMovieDBr);
+if(personalMovieDB.count<10){
+    alert("просмотрено довольно мало фильмов");
+} else if(personalMovieDB.count>=10 && personalMovieDB.count<=30){
+    alert("Вы классический зритель");
+}else if(personalMovieDB.count>30){
+    alert('вы киноман');
+}else {
+    alert('Произошла ошибка');
+}
